f3dbf069e9f936edad917f70c9500798098c2389
Daniela Pedon <danielapedon3@gmail.com>
2021-04-12 18:00:23 -0300

    Special move - Castling

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -53,7 +53,7 @@ public class Pawn extends ChessPiece{
 			p.setValues(position.getRow() + 1, position.getColumn() + 1);
 			if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 				mat[p.getRow()][p.getColumn()] = true;
-			}
+			}																																						
 			
 		}
 		
