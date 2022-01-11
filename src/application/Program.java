0d938ef1b058172714dad75964898a143440151b
Daniela Pedon <danielapedon3@gmail.com>
2022-01-11 18:53:36 -0300

    Special move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,6 +37,16 @@ public class Program {
                 if (capturedPiece != null) {
                     captured.add(capturedPiece);
                 }
+                if (chessMatch.getPromoted() != null) {
+                    System.out.print("Enter piece for promotion (B/N/R/Q): ");
+                    String type = sc.nextLine().toUpperCase();
+                    while (!type.equals("B") && !type.equals("N") && !type.equals("R") & !type.equals("Q")) {
+                        System.out.print("Invalid value! Enter piece for promotion (B/N/R/Q): ");
+                        type = sc.nextLine().toUpperCase();
+                    }
+                    chessMatch.replacePromotedPiece(type);
+                }
+
             } catch (ChessException e) {
                 System.out.println(e.getMessage());
                 sc.nextLine();
@@ -47,8 +57,7 @@ public class Program {
 
         }
         UI.clearScreen();
-        UI.printMatch(chessMatch,captured);
-
+        UI.printMatch(chessMatch, captured);
 
 
     }
